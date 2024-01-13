31eeae824ace523fa862c27aef1d3ec033584df2
Emilia Prusevicius <emilia.prusevicius@playbookengineering.com>
2024-01-13 18:46:27 +0100

    016 - local storage functionality

--- src/components/exerciseList/ExerciseFormState.ts
+++ src/components/exerciseList/ExerciseFormState.ts
@@ -3,6 +3,7 @@ import { action } from 'mobx';
 import { InputState } from '../input/InputState';
 import { createExercise, updateExercise } from '../../api/exercises';
 import { ExerciseType } from './ExercisesState';
+import { getImageFromIndexedDB, saveImageToIndexedDB } from '../../localStorage/exercises';
 
 export type ExerciseCreateType = Omit<ExerciseType, 'id' | 'imgUrl'> & { imgUrl: File | null };
 
@@ -14,6 +15,7 @@ export class ExerciseFormState {
     @observable public isActive: boolean = this.exercise?.isActive ?? true;
     @observable public isFavorite: boolean = this.exercise?.isFavorite ?? false;
     @observable public isClearImgForm: boolean = false;
+    @observable public blabla: string = '';
 
     public constructor(
         private exercise: ExerciseType | null,
@@ -63,6 +65,7 @@ export class ExerciseFormState {
     }
 
     @action handleCreateExercise = async (): Promise<void> => {
+        await this.saveImage();
         const data = {
             label: this.label.value,
             imgUrl: this.imgFile.value,
@@ -70,6 +73,20 @@ export class ExerciseFormState {
             isActive: this.isActive,
             isFavorite: this.isFavorite
         }
+        const dataLocal = {
+            label: this.label.value,
+            imgUrl: this.blabla,
+            isBothSides: this.isBothSides,
+            isActive: this.isActive,
+            isFavorite: this.isFavorite
+        }
+
+        const exercisesLocal = JSON.parse(localStorage.getItem('exercisesLocal') || '[]');
+        exercisesLocal.push(dataLocal);
+        localStorage.setItem('exercisesLocal', JSON.stringify(exercisesLocal));
+        // this.getExerciseList();
+        // this.closePopup();
+
         try {
             await createExercise(data);
             this.clearForm();
@@ -81,6 +98,7 @@ export class ExerciseFormState {
     }
 
     @action handleUpdateExercise = async (): Promise<void> => {
+        await this.saveImage();
         const data = {
             label: this.label.value,
             imgUrl: this.imgFile.value,
@@ -88,8 +106,27 @@ export class ExerciseFormState {
             isActive: this.isActive,
             isFavorite: this.isFavorite
         }
-
         if (this.id !== null) {
+            const dataLocal = {
+                id: this.id,
+                label: this.label.value,
+                imgUrl: this.blabla,
+                isBothSides: this.isBothSides,
+                isActive: this.isActive,
+                isFavorite: this.isFavorite
+            }
+
+            try {
+                const exercisesLocal: Array<ExerciseType> = JSON.parse(localStorage.getItem('exercises') || '[]');
+                const index = exercisesLocal.findIndex(e => e.id === this.id);
+                if (index !== -1) {
+                    exercisesLocal[index] = dataLocal;
+                    localStorage.setItem('exercises', JSON.stringify(exercisesLocal));
+                }
+                } catch (error) {
+                    console.log('Error processing data', error);
+            }
+
             try {
                 await updateExercise(this.id, data);
                 this.clearForm();
@@ -100,4 +137,43 @@ export class ExerciseFormState {
             }
         }
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @action handleCreateNewwwwwww = async (): Promise<void> => {
+        const data = {
+            label: this.label.value,
+            imgUrl: this.imgFile.value,
+            isBothSides: this.isBothSides,
+            isActive: this.isActive,
+            isFavorite: this.isFavorite
+        }
+        try {
+            await this.saveImage();
+        } catch (error) {
+            console.log('Error fetching data')
+        }
+    }
+
+    @action saveImage = async (): Promise<void> => {
+        if (this.imgFile.value) {
+            const imageId = 'exerciseImage_' + Date.now(); // Unique ID for the image
+            await saveImageToIndexedDB(imageId, this.imgFile.value);
+            // localStorage.setItem(this.label.value, imageId); // Store reference with label as key
+            // update exercises?
+        }
+        this.loadImage('test');
+    };
+
+    // New method to load an image from IndexedDB using the reference in Local Storage
+    loadImage = async (label: string): Promise<string | null> => {
+        const imageId = localStorage.getItem(label);
+        if (imageId) {
+            const imageBlob = await getImageFromIndexedDB(imageId);
+            this.blabla = URL.createObjectURL(imageBlob);
+            console.log(URL.createObjectURL(imageBlob));
+            return URL.createObjectURL(imageBlob); // Convert Blob to URL for display
+        }
+        return null;
+    };
 }
