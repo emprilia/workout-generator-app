9331487d4237caa484b52fb6a4a62e6f584aeaf6
Emilia Prusevicius <emilia.prusevicius@playbookengineering.com>
2024-05-03 22:13:59 +0200

    034 - handle finished workout logic

--- src/components/slider/exerciseSlider/ExerciseSlider.tsx
+++ src/components/slider/exerciseSlider/ExerciseSlider.tsx
@@ -17,9 +17,14 @@ import {
     SpeakerMuteIconWrapper,
     ImgPlaceholder,
     RoundLabel,
-    PauseIconWrapper
+    PauseIconWrapper,
+    FinishedWrapper,
+    FinishedText,
+    FinishedControlsWrapper,
+    SuccessIconWrapper
 } from './ExerciseSlider.style';
 import { CounterIconWrapper } from '../../counter/Counter.style';
+import { Button } from '../../button/Button';
 
 interface ExerciseSliderPropsType {
     exercisesState: ExercisesState;
@@ -30,7 +35,7 @@ interface ExerciseSliderPropsType {
 export const ExerciseSlider = observer((props: ExerciseSliderPropsType) => {
     const { exercisesState, sliderState, counterState } = props;
 
-    const { 
+    const {
         time,
         runTimer,
         hasStarted,
@@ -43,7 +48,10 @@ export const ExerciseSlider = observer((props: ExerciseSliderPropsType) => {
         isPrepTime,
         isBreakTime,
         isWorkoutTime,
-        generateWorkout
+        isWorkoutFinished,
+        generateWorkout,
+        handleGoAgain,
+        handleGoNew
     } = counterState;
 
     const {
@@ -57,34 +65,44 @@ export const ExerciseSlider = observer((props: ExerciseSliderPropsType) => {
 
     return (
         <ExerciseContainer width={sliderWidth}>
-            <ExerciseHeader>
-                <CounterIconWrapper time={time} />
-                {
-                    isPrepTime ? <RoundLabel>GET READY</RoundLabel> :
-                    isBreakTime ? <RoundLabel>NEXT</RoundLabel> :
-                    isWorkoutTime ? <RoundLabel>GO</RoundLabel> :
-                    null
-                }
-                <ControlsWrapper>
-                    {hasPaused ? <>
-                        <PlayIconWrapper onClick={resumeTimer} />
-                        <StopIconWrapper onClick={stopTimer} />
-                    </> : hasStarted ? <>
-                        {isPrepTime ? null : <PauseIconWrapper onClick={pauseTimer} />}
-                        <StopIconWrapper onClick={stopTimer} />
-                    </> : <PlayIconWrapper onClick={runTimer} />}
-                    <RefreshIconWrapper onClick={generateWorkout} />
-                    {isMuted ? <SpeakerMuteIconWrapper onClick={setIsMuted} /> : <SpeakerIconWrapper onClick={setIsMuted} />}
-                </ControlsWrapper>
-            </ExerciseHeader>
-            <ExerciseList translateX={translateSlide}>
-                {generatedWorkout.map((exercise) => (
-                    <Exercise key={exercise.label} width={sliderWidth}>
-                        {exercise.imgUrl === null ? <ImgPlaceholder /> : <ExerciseImg src={exercise.imgUrl} alt={`Picture of ${exercise.label} exercise`} />}
-                        <ExerciseLabel>{exercise.label}</ExerciseLabel>
-                    </Exercise>
-                ))}
-            </ExerciseList>
+            {isWorkoutFinished ? <FinishedWrapper>
+                <FinishedText>WELL DONE!</FinishedText>
+                <SuccessIconWrapper />
+                <FinishedControlsWrapper>
+                    <Button size='small' onClick={handleGoAgain}>Do it again?</Button>
+                    <Button size='small' onClick={handleGoNew}>Start new?</Button>
+                </FinishedControlsWrapper>
+                <div onClick={handleGoNew}>End for today</div>
+            </FinishedWrapper> : <>
+                <ExerciseHeader>
+                    <CounterIconWrapper time={time} />
+                    {
+                        isPrepTime ? <RoundLabel>GET READY</RoundLabel> :
+                        isBreakTime ? <RoundLabel>NEXT</RoundLabel> :
+                        isWorkoutTime ? <RoundLabel>GO</RoundLabel> :
+                        null
+                    }
+                    <ControlsWrapper>
+                        {hasPaused ? <>
+                            <PlayIconWrapper onClick={resumeTimer} />
+                            <StopIconWrapper onClick={stopTimer} />
+                        </> : hasStarted ? <>
+                            {isPrepTime ? null : <PauseIconWrapper onClick={pauseTimer} />}
+                            <StopIconWrapper onClick={stopTimer} />
+                        </> : <PlayIconWrapper onClick={runTimer} />}
+                        <RefreshIconWrapper onClick={generateWorkout} />
+                        {isMuted ? <SpeakerMuteIconWrapper onClick={setIsMuted} /> : <SpeakerIconWrapper onClick={setIsMuted} />}
+                    </ControlsWrapper>
+                </ExerciseHeader>
+                <ExerciseList translateX={translateSlide}>
+                    {generatedWorkout.map((exercise) => (
+                        <Exercise key={exercise.label} width={sliderWidth}>
+                            {exercise.imgUrl === null ? <ImgPlaceholder /> : <ExerciseImg src={exercise.imgUrl} alt={`Picture of ${exercise.label} exercise`} />}
+                            <ExerciseLabel>{exercise.label}</ExerciseLabel>
+                        </Exercise>
+                    ))}
+                </ExerciseList>
+            </>}
         </ExerciseContainer>
     );
 });
